021caeb3801f5a790cabc18b89d2769763099396
Manya Khullar <manyakhullar457@gmail.com>
2024-11-03 21:41:17 -0500

    Initial commit with project setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="React Logo" />
+        <h1>Welcome to Fullstack Development - I</h1>
+        <h2>React JS Programming Week09 Lab Exercise</h2>
+        <p>ID: 101410731</p>
+        <p>Manya Khullar</p>
+        <p>George Brown College, Toronto</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
